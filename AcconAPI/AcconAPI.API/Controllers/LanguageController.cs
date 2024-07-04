f0273f3f3c60d74fd1d5181ddb461bef22fda145
Hakan Yaman <hakan.yaman@d-teknoloji.com.tr>
2024-07-04 22:21:32 +0300

    add Language and SocialMedia controller

---
+++ AcconAPI/AcconAPI.API/Controllers/LanguageController.cs
@@ -0,0 +1,34 @@
+﻿using AcconAPI.Application.Features.Commands.Language;
+using AcconAPI.Application.Features.Queries.Languages;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcconAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LanguageController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UpdateLanguage([FromBody] UpdateLanguageCommandRequest command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllLanguages()
+        {
+            var result = await _mediator.Send(new GetAllLanguagesQueryRequest());
+            return Ok(result);
+        }
+    }
+}
---
+++ AcconAPI/AcconAPI.API/Controllers/SocialController.cs
@@ -0,0 +1,34 @@
+﻿using AcconAPI.Application.Features.Commands.SocialMedia;
+using AcconAPI.Application.Features.Queries.SocialMedia;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcconAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocialController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SocialController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> UpdateSocial([FromBody] UpdateSocialMediaCommandRequest request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllSocial()
+        {
+            var result = await _mediator.Send(new GetAllSocialMediaQueryRequest());
+            return Ok(result);
+        }
+    }
+}
